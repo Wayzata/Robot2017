553bbae1327d21e5a5f61c9a4995f129828eb9de
pidatpre000 <pidatpre000@isd284.com>
2017-02-06 18:14:33 -0600

    updates
    updates

--- src/org/usfirst/frc/team2264/robot/Shooter.java
+++ src/org/usfirst/frc/team2264/robot/Shooter.java
@@ -1,5 +1,36 @@
 package org.usfirst.frc.team2264.robot;
 
-public class Shooter {
+import com.ctre.CANTalon;
+import edu.wpi.first.wpilibj.buttons.Button;
 
+public class Shooter {
+	
+	CANTalon shooterMotor;
+	CANTalon feederMotor;
+	Button rTrigger;
+	Button lTrigger;
+	
+	public void ShooterInit(){
+		shooterMotor = new CANTalon(RobotMap.shooterMotor);
+		feederMotor = new CANTalon(RobotMap.feederMotor);
+		rTrigger = new Button(RobotMap.GamepadButtons.RTriggerNumber);
+		lTrigger = new Button(RobotMap.GamepadButtons.LTriggerNumber);
+	}
+	
+	public void MotorOn(){
+		if (rTrigger){
+			shooterMotor.set(.5);
+		}
+		else{
+			shooterMotor.set(0);
+		}
+	}
+	public void helperOn(){
+		if (lTrigger){
+			feederMotor.set(.5);
+		}
+		else{
+			feederMotor.set(0);
+		}
+	}
 }
