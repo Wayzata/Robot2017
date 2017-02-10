4940c2fcc03d0dfbac92316606d005bf560f3750
Preeti Pidatala <pidatpre000@isd284.com>
2017-02-09 20:07:19 -0600

    edits

--- src/org/usfirst/frc/team2264/robot/UltrasonicSensor.java
+++ src/org/usfirst/frc/team2264/robot/UltrasonicSensor.java
@@ -1,5 +1,17 @@
 package org.usfirst.frc.team2264.robot;
 
+import edu.wpi.first.wpilibj.Ultrasonic;
+
 public class UltrasonicSensor {
 
+	public UltrasonicSensor(){
+		Ultrasonic sensor;
+		
+	}
+	
+	public void sense(){
+
+	}
+	
+	
 }
