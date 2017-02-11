f4fe18296f52faf7174a5ff497471c2fd1b43970
pidatpre000 <pidatpre000@isd284.com>
2017-02-10 18:14:08 -0600

    Added class sensor for the Ultrasonic

---
+++ src/org/usfirst/frc/team2264/robot/Sensor.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team2264.robot;
+
+import edu.wpi.first.wpilibj.Ultrasonic;
+
+public class Sensor {
+	
+	Ultrasonic uSensor;
+	
+	public Sensor(){
+		
+		uSensor = new Ultrasonic(2, 2);
+		
+	}
+
+	public 
+	
+}
+
+/* if the sensor 
